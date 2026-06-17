d0372903baaf304947c268f55ef693dd93dd3fc6
asherpert <asherperttula@gmail.com>
2026-06-16 21:19:52 -0700

    death system p1

--- game/mouse_char.gd
+++ game/mouse_char.gd
@@ -26,14 +26,16 @@ func _physics_process(delta: float) -> void:
 			
 
 	move_and_slide()
-	var Area=get_node("%CutoutWasp").get_node("Area")
+	var Area=get_node("%CutoutWasp").get_node("Area") as Area2D
 	var body=get_node("%CutoutWasp").get_node("%Wasp3Body")
 	body.get_node("wasp3legs").ampdeg = 0
 	body.get_node("wasp3legs").periodsec = 0
 	var now = Time.get_unix_time_from_system()
-	if Area.has_overlapping_bodies():
-		if wasp_hit_time == 0.0:
-			wasp_hit_time = now
+	for i in Area.get_overlapping_bodies():
+		if i.name == "wasparea":
+
+			if wasp_hit_time == 0.0:
+				wasp_hit_time = now
 	else:
 		if wasp_hit_time > 0.0 and now > wasp_hit_time + 2.0:
 			wasp_hit_time = 0.0
